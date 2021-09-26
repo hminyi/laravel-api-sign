5286fa4f2de7bf0b926e6368fb043918cb89fc10
Sirius <707759383@qq.com>
2021-09-26 16:04:26 +0800

    配置错误

--- config/signature.php
+++ config/signature.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'status' => env('APP_SIGN_ON', true),
+    'status' => env('APP_SIGN_ON', true), // 是否开启验证
     'apps'   => [
-        'app_id'     => '',
-        'app_secret' => '',
-        'timestamp'  => 5 * 60, // 默认5分钟
-        'status'     => 1, // 是否禁用
+        [
+            'app_id'     => '',
+            'app_secret' => '',
+            'timestamp'  => 5 * 60, // 默认5分钟
+            'status'     => 1, // 是否禁用
+        ],
     ],
 ];
